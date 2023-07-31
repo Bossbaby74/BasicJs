01e4b9bf653b4e852340183470f8fcd55bb68365
Nzengang <gabriel588@rocketmail.com>
2023-07-31 14:43:40 +0100

    calculating volume with user input

--- userInteraction.js
+++ userInteraction.js
@@ -3,11 +3,11 @@ export function jsVolume(){
     let width = prompt("Enter the with");
     let height = prompt("Enter the height");
     let length = prompt("Enter the length");
-    width = number(width);
-    height = number(height);
-    length = number(length);
+    width = Number(width);
+    height = Number(height);
+    length = Number(length);
 
-    if(length || width || height == NaN){
+    if(length || width || height == NaN || "String"){
         console.log("Please enter a valid number");
     }
 
