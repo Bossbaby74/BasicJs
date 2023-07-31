9cac0c509dee818ba5822c29d37d3ad7ec8fe3d3
Nzengang <gabriel588@rocketmail.com>
2023-07-31 14:11:16 +0100

    changes: creating each file for a particular lesson

---
+++ jsObjects1.js
@@ -0,0 +1,21 @@
+//excercise1 on objects
+export const ticket = {
+    from :  "Starting station",
+    to : "Ending station",
+    price : "$" + 50
+};
+
+console.log(`ticket info: departure: ${ticket.from}`);  console.log(`Destination: ${ticket.to}`); console.log(`price: ${ticket.price}`);
+
+/*creating an object called ticket that will display the departure point and the destination.*/
+
+// excercise 2 on obj
+
+let person = {
+
+};
+
+person.name = "Nzengang";
+person.surName = "Gabriel";
+console.log(`${person.name}`); console.log(`${person.surName}`);
+//creating a person object that will receive a name and surname and then display them
---
+++ jsflowers.js
@@ -0,0 +1,21 @@
+//exercise:
+export function flowers(){ 
+let NoOfRoses = 70;
+let NoOfLily = 50;
+let NoOfTulip = 120;
+
+let RosePrice = 8;
+let LilyPrice = 10;
+let TulipPrice = 2;
+
+let RoseValue = RosePrice * NoOfRoses;
+let LilyValue = LilyPrice * NoOfLily;
+let TulipValue = TulipPrice * NoOfTulip;
+
+let Total = RoseValue + LilyValue + TulipValue;
+
+console.log("Rose - unit price: " + RosePrice + ", quantity: " + NoOfRoses + ", Value: " + RoseValue);
+console.log("Lily - unit price: " + LilyPrice + ", quantity: " + NoOfLily + ", Value: " + LilyValue);
+console.log("Tulip - unit price: " + TulipPrice + ", quantity: " + NoOfTulip + ", Value: " + TulipValue);
+console.log("Total: " + Total );
+}
